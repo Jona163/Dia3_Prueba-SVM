f9c39c8b807154a881c67bfa596691af2a3955e8
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-04 20:34:52 -0600

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -19,3 +19,43 @@ class SVM:
         self.iteraciones = iteraciones
         self.w = None  # Pesos
         self.b = None  # Sesgo
+
+    def ajustar(self, X, y):
+        """
+        Ajusta el modelo SVM a los datos de entrada X y las etiquetas y.
+
+        Parámetros:
+        - X: matriz de características de tamaño (n_muestras, n_características).
+        - y: vector de etiquetas de tamaño (n_muestras).
+        """
+        n_muestras, n_caracteristicas = X.shape
+        y_ = np.where(y <= 0, -1, 1)  # Reasigna etiquetas a -1 y 1
+
+        # Inicialización de pesos y sesgo
+        self.w = np.zeros(n_caracteristicas)
+        self.b = 0
+
+        # Descenso de gradiente para ajustar los pesos
+        for _ in range(self.iteraciones):
+            for idx, x_i in enumerate(X):
+                condicion = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condicion:
+                    # Actualización en caso de que el margen sea mayor o igual a 1
+                    self.w -= self.tasa_aprendizaje * (2 * self.parametro_lambda * self.w)
+                else:
+                    # Actualización en caso contrario
+                    self.w -= self.tasa_aprendizaje * (2 * self.parametro_lambda * self.w - np.dot(x_i, y_[idx]))
+                    self.b -= self.tasa_aprendizaje * y_[idx]
+
+    def predecir(self, X):
+        """
+        Predice las etiquetas para las muestras de entrada X.
+
+        Parámetros:
+        - X: matriz de características de tamaño (n_muestras, n_características).
+
+        Retorna:
+        - Predicciones: vector de etiquetas predichas.
+        """
+        aproximacion = np.dot(X, self.w) - self.b
+        return np.sign(aproximacion)
